3e7e3c5b41e57ed78987c44fdc6dabdd3e265dec
RaZaki <Rayef.zaki@gmail.com>
2022-12-10 23:08:29 +0300

    add cors

--- src/app.ts
+++ src/app.ts
@@ -5,16 +5,18 @@ import userRouter from './router/User.router';
 import ticketRouter from './router/Ticket.router';
 import 'dotenv/config';
 connectDB();
+import cors from 'cors';
+
 
 const app = express();
 app.use(express.json())
 // const port = 3000;
-
+app.use(cors());
 
 app.use('/api/v1/auth',userRouter);
 app.use('/api/v1/ticket',ticketRouter);
 
-const port = process.env.port || 3000;
+const port = process.env.port || 5000;
 app.listen(port,()=>{
     console.log(`Server is ${port}`)
 })
